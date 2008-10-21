b4d930ffe9c7cd59b3285f2681262917d6d2f740
Elan Ruusamäe <glen@pld-linux.org>
2008-10-21 23:07:04 +0000

    - find locale files from ZendFramework
    Changed files:
        ZendFramework-find-lang.sh -> 1.1

---
+++ ZendFramework-find-lang.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+dir=$RPM_BUILD_ROOT/usr/share/pear/Zend/Locale/Data
+langfile=${1:-ZendFramework.lang}
+
+> $langfile
+find $dir -type f -name '*.xml' | while read file; do
+	file=${file#$RPM_BUILD_ROOT}
+
+	case "${file##*/}" in
+	supplementalData.xml)
+		# skip - no lang tag for this one
+		continue
+		;;
+	esac
+
+	language=$(sed -ne 's/<language type="\(.*\)"\/>/\1/p' $file | xargs)
+	script=$(sed -ne 's/<script type="\(.*\)"\/>/\1/p' $file | xargs)
+	territory=$(sed -ne 's/<territory type="\(.*\)"\/>/\1/p' $file | xargs)
+
+	#<language type="sr"/>
+	#<script type="Latn"/>
+	#<territory type="BA"/>
+	# sr_Latn_BA.xml -> sr_BA@Latn
+
+	# TODO: <variant type="SAAHO"/>
+
+	lang=$language${territory:+_$territory}${script:+@$script}
+	echo "%lang($lang) ${file#$RPM_BUILD_ROOT} # $o" >> $langfile
+done
